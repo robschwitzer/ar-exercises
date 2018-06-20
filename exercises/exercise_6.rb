6d0be4fb55274bcaf6414ccbcab7ee13d5b07c77
Rob Schwitzer <deadinthewater@me.com>
2018-06-20 03:57:42 +0000

    gave employees a raise

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,12 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
-@store1.employees.create(first_name: "Scooby", last_name: "Doo", hourly_rate: 25)
-@store1.employees.create(first_name: "Scrappy", last_name: "Doo", hourly_rate: 25)
-@store1.employees.create(first_name: "Floopy", last_name: "Doo", hourly_rate: 25)
-@store1.employees.create(first_name: "Flabby", last_name: "Doo", hourly_rate: 25)
-@store2.employees.create(first_name: "Stinky", last_name: "Doo", hourly_rate: 25)
-@store2.employees.create(first_name: "Moopy", last_name: "Doo", hourly_rate: 25)
-@store2.employees.create(first_name: "Bruce", last_name: "Doo", hourly_rate: 25)
-@store2.employees.create(first_name: "Poopy", last_name: "Doo", hourly_rate: 25)
-@store2.employees.create(first_name: "Steve", last_name: "Doo", hourly_rate: 25)
+@store1.employees.create(first_name: "Scooby", last_name: "Doo", hourly_rate: 50)
+@store1.employees.create(first_name: "Scrappy", last_name: "Doo", hourly_rate: 50)
+@store1.employees.create(first_name: "Floopy", last_name: "Doo", hourly_rate: 50)
+@store1.employees.create(first_name: "Flabby", last_name: "Doo", hourly_rate: 50)
+@store2.employees.create(first_name: "Stinky", last_name: "Doo", hourly_rate: 50)
+@store2.employees.create(first_name: "Moopy", last_name: "Doo", hourly_rate: 50)
+@store2.employees.create(first_name: "Bruce", last_name: "Doo", hourly_rate: 50)
+@store2.employees.create(first_name: "Poopy", last_name: "Doo", hourly_rate: 50)
+@store2.employees.create(first_name: "Steve", last_name: "Doo", hourly_rate: 50)

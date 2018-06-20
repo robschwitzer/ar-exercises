1f81ab85c946fd1572441ce6546b27b2789c8aea
Rob Schwitzer <deadinthewater@me.com>
2018-06-20 02:24:44 +0000

    added some new stores. got data on mens and womens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Surrey   = Store.create(name: "Surrey", mens_apparel: false, womens_apparel: true, annual_revenue: 224000)
+Whistler = Store.create(name: "Whistler", mens_apparel: true, womens_apparel: false, annual_revenue: 1900000)
+Yaletown = Store.create(name: "Yaletown", mens_apparel: true, womens_apparel: true, annual_revenue: 430000)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name
+  puts store.annual_revenue
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.each do |store|
+  if (store.annual_revenue < 1000000)
+    puts store.name
+  end
+end

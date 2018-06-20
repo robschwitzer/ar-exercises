4065f52d33ef4d30d5ca404b5dc6d00fc67d25eb
Rob Schwitzer <deadinthewater@me.com>
2018-06-20 01:42:31 +0000

    created 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,8 +5,8 @@ puts "----------"
 
 # Your code goes below here ...
 
-Burnaby = Store.create(name: "Burnaby", mens_apparel: true, womens_apparel: true, annual_revenue: 300000);
+Burnaby  = Store.create(name: "Burnaby", mens_apparel: true, womens_apparel: true, annual_revenue: 300000);
 Richmond = Store.create(name: "Richmond", mens_apparel: false, womens_apparel: true, annual_revenue: 1260000);
-Gastown = Store.create(name: "Gastown", mens_apparel: true, womens_apparel: false, annual_revenue: 190000);
+Gastown  = Store.create(name: "Gastown", mens_apparel: true, womens_apparel: false, annual_revenue: 190000);
 
 puts Store.count
